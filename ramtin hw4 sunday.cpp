87b9f4526bd1e189322868a19c5db24abbb1146d
Ramtin <149162748+Ramtin1402@users.noreply.github.com>
2023-12-02 09:41:49 +0330

    Ramtin Rahimi hw4 : 40213841054090

---
+++ ramtin hw4 sunday.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+ int main() {
+	float a,b;
+	cout << "please enter first number :";
+	cin >> a;
+	cout << "please enter second number :";
+	cin >> b;
+	cout << "a + b =  :" << a + b ;
+	cout << "\n a - b = :" << a - b ;
+	cout << "\n a * b = :" << a * b ;
+	cout << "\n a / b = :" << (int) a / (int) b ;
+}
